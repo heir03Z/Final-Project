c4b1566087f1f7556e3955543a42872a3692a441
Emmy Ma <hma@babson.edu>
2021-04-26 14:06:03 -0400

    update

--- Python Project/app1.py
+++ Python Project/app1.py
@@ -17,13 +17,17 @@ app.config['SECRET_KEY'] = os.urandom(24)
 app.config['SESSION_TYPE'] = 'filesystem'
 
 
-# create a database to store user information
+# create a database to store user information and sentences
 db = SQLAlchemy()
 DB_NAME = 'database.db'
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
 db.init_app(app)
 
 class User(db.Model, UserMixin):
+    """
+    Define a User model to store the username and password 
+    that new user creates via the form
+    """
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(25), unique=True)
     password = db.Column(db.String(25))
@@ -32,9 +36,13 @@ class User(db.Model, UserMixin):
         return '<User %r>' % self.username
 
 class Sentences(db.Model):
+    """
+    Define a Sentences model to store the sentence 
+    that users enter via the form
+    """
     __tablename__ = 'sentences'
     id = db.Column(db.Integer, primary_key=True)
-    sentence = db.Column(db.String(1000))
+    sentence = db.Column(db.String(1000)) #The maximum characters that users can enter is 1000
     pub_date = db.Column(db.DateTime)
 
     # Initializes the fields with entered data
@@ -48,16 +56,21 @@ with app.app_context():
 @app.route('/')
 def index():
     """
-    Index Page
+    Index Page: displaying all the sentences ordered in 
+    ascending order of post time
+    sentences: all the sentences that users enter
     """
     return render_template('index.html', sentences=Sentences.query.order_by(Sentences.pub_date.asc()).all())
 
 @app.route('/create', methods=['POST', 'GET'])
 def create():
     """
-    page for creating the story
+    create page: get sentences from users via form
+    if the information is valid then redirect back to index page
     """
     form = AddForm(request.form)
+    # check if the information entered in the form is valid
+    # if valid then store the sentence in the database
     if request.method == 'POST' and form.validate():
         sentence = Sentences(sentence=form.sentence.data)
         db.session.add(sentence)
@@ -67,11 +80,14 @@ def create():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
-    """Create a registration page"""
+    """
+    register page: get user information from new users via form
+    if the information is valid then redirect to login page
+    """
     form = RegistrationForm(request.form)
-    if request.method == 'POST' and form.validate():
+    if request.method == 'POST' and form.validate(): # check if the information entered in the form is valid
         flash('The account is created, please log in')
-        new_user = User(username=form.username.data, password=form.psw.data)
+        new_user = User(username=form.username.data, password=form.psw.data) # store the information in the database
         db.session.add(new_user)
         db.session.commit()
         return redirect(url_for('login'))
@@ -79,12 +95,15 @@ def register():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    """Create a login page"""
+    """
+    login page: get user information from new users via form
+    if the information is valid then redirect to create page
+    """
     form = LoginForm(request.form)
     if request.method == 'POST' and form.validate():
         user = User.query.filter_by(username=form.username.data).first()
         if user:
-            if user.password == form.psw.data:
+            if user.password == form.psw.data: # check if the username and password entered match
                 return redirect(url_for('create'))
     return render_template('login.html', form = form)
 
